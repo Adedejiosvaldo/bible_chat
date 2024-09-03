d98df88ca3e4d3f90addd97d2f246ea03e068d71
Joseph Adewunmi <adedejiosvaldo@gmail.com>
2024-09-03 13:23:04 +0100

    Fix -> Removed wrong import

--- app/api/auth/token/route.ts
+++ app/api/auth/token/route.ts
@@ -1,8 +1,8 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest } from "next";
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextApiRequest) {
   const token = await getToken({ req });
   return NextResponse.json({ token });
 }
