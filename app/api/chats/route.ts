8b9d5b9d7d76fa3a658c5f1a0a27a3672f8e859b
Joseph Adewunmi <adedejiosvaldo@gmail.com>
2024-09-22 20:12:20 +0100

    Fix -> Refactoring Prisma

--- app/api/chats/route.ts
+++ app/api/chats/route.ts
@@ -10,7 +10,7 @@ export async function GET(request: NextRequest) {
     if (!session || !session.user) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-
+    //
     const chats = await prisma.chat.findMany({
       where: {
         user: {
